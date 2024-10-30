79b774f40d3512cc8898c003b1b38b954ca4864b
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-10-30 12:42:07 -0400

    Test console.log

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -10,6 +10,7 @@ export const CrossfeedFooter: React.FC = (props) => {
   const FooterRoot = FooterStyles.FooterRoot;
   const footerClasses = FooterStyles.footerClasses;
   const versionNumber = process.env.REACT_APP_VERSION_NUMBER;
+  console.log('versionNumber', versionNumber);
 
   return (
     <FooterRoot>
