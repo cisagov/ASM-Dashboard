d5d54208fcdb89c32afe1da73c23f10526f1d392
nickviola <nick.viola@associates.cisa.dhs.gov>
2024-11-14 15:26:52 -0600

    Fix api-keys endpoint by updating api method response and schema model

--- backend/src/xfd_django/xfd_api/schema_models/api_key.py
+++ backend/src/xfd_django/xfd_api/schema_models/api_key.py
@@ -7,6 +7,35 @@ from uuid import UUID
 # Third-Party Libraries
 from pydantic import BaseModel, ConfigDict
 
+# class ApiKey(BaseModel):
+#     """Pydantic model for the ApiKey model."""
+
+#     id: UUID
+#     createdAt: datetime
+#     updatedAt: datetime
+#     lastUsed: Optional[datetime]
+#     hashedKey: Optional[str]
+#     lastFour: Optional[str]
+#     userId: Optional[UUID]
+
+#     @classmethod
+#     def model_validate(cls, obj):
+#         # Ensure that we convert the UUIDs to strings when validating
+#         api_key_data = obj.__dict__.copy()
+
+#         # Remove the '_state' field or any other unwanted internal Django fields
+#         api_key_data.pop("_state", None)
+#         api_key_data["userId"] = api_key_data.pop("userId_id", None)
+
+#         for key, val in api_key_data.items():
+#             # Convert any UUIDs to strings
+#             if isinstance(val, UUID):
+#                 api_key_data[key] = str(val)
+#         return cls(**api_key_data)
+
+#     class Config:
+#         from_attributes = True
+
 
 class ApiKey(BaseModel):
     """Pydantic model for the ApiKey model."""
@@ -15,16 +44,21 @@ class ApiKey(BaseModel):
     createdAt: datetime
     updatedAt: datetime
     lastUsed: Optional[datetime]
-    hashedKey: Optional[str]
+    hashedKey: Optional[
+        str
+    ] = None  # Make optional to avoid validation error in response
     lastFour: Optional[str]
-    userId: Optional[UUID]
+    userId: Optional[UUID] = None  # Make optional to avoid validation error in response
+    api_key: Optional[
+        str
+    ] = None  # Add the actual key to the schema for response inclusion
 
     @classmethod
     def model_validate(cls, obj):
         # Ensure that we convert the UUIDs to strings when validating
         api_key_data = obj.__dict__.copy()
 
-        # Remove the '_state' field or any other unwanted internal Django fields
+        # Remove the '_state' field or any other unwanted internal fields
         api_key_data.pop("_state", None)
         api_key_data["userId"] = api_key_data.pop("userId_id", None)
 
