36eb23cd50300ef8868f1afe6d25c8331c514c78
Thomas Johnson <123108455+hawkishpolicy@users.noreply.github.com>
2024-11-18 12:58:06 -0500

    Added Severity Label validation to All Vulns table (CRASM-822) (#720)
    * Added Severity Label validation to All Vulns table
    
    - Ensured that null or empty string values are displayed as 'N/A' in the severity column of the All Vulns table.
    - Ensured that values of Other or any value that does not include 'Critical', 'High', 'Medium', or 'Low' are displayed as 'Other' in the severity column of the All Vulns table..
    - Formatted the severity column of the All Vulns table to display the severity label in title case.
    - Updated the vulnerability search endpoint to return vulnerabilities with a severity of 'Other' if the severity is not 'Critical', 'High', 'Medium', or 'Low'.
    - Updated the vulnerability search endpoint to return vulnerabilities with a severity of 'N/A' if the severity is null or an empty string.
    - Updated the vulnerability search endpoint to return vulnerabilities regardless of the case of the severity value.
    
    * Resolved issue with frontend snapshot
    
    - Could be from the HTML encodings for the org search introduced in CRASM-746/PR #691
    
    * Reverted edit to RegionAndOrganizationFilters.tsx
    
    - Added an if statement and a console.log to help troubleshoot a github actions issue.
    - This change was reverted because it was erroneously included in the draft pull request.
    
    * Resolve fixes
    
    ---------
    
    Co-authored-by: Janson Bunce <janson.bunce@associates.cisa.dhs.gov>

--- frontend/src/components/RegionAndOrganizationFilters.tsx
+++ frontend/src/components/RegionAndOrganizationFilters.tsx
@@ -98,6 +98,7 @@ export const RegionAndOrganizationFilters: React.FC<
             regions
           }
         });
+
         const orgs = results.body.hits.hits.map((hit) => hit._source);
         // Filter out organizations that match the exclusions
         const refinedOrgs = orgs.filter((org) => {
