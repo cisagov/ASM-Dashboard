e8424a15b11a5eb9e18e4211b002d9cb44a81817
cduhn17 <craig.duhn@nttdatafed.com>
2025-01-24 07:18:36 -0600

    Update esling.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,26 @@
-// This file is used to configure the ESLint rules
-// for the project to find unused code.
+import { defineConfig } from 'eslint-define-config';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 
-export default [
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tsParser
+export default defineConfig({
+  parser: tsParser,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'error'
-    }
+    project: './tsconfig.json'
+  },
+  plugins: {
+    '@typescript-eslint': tsPlugin
+  },
+  extends: [
+    'react-app',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    // Add or override other rules as needed
   }
-];
+});
