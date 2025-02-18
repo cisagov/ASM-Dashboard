9eb11de15d131bbae601835fabe23dca64a05e90
DJensen94 <79864006+DJensen94@users.noreply.github.com>
2025-02-18 14:47:45 -0700

    run linter on WAS sync
    run linter on WAS sync

--- backend/src/xfd_django/xfd_api/tasks/was_sync.py
+++ backend/src/xfd_django/xfd_api/tasks/was_sync.py
@@ -9,7 +9,6 @@ import django
 import requests
 from xfd_mini_dl.models import DataSource, Organization, WasFindings, WasReport
 
-
 # Django setup
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xfd_django.settings")
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
@@ -23,7 +22,6 @@ TIMEOUT = 60  # Timeout in seconds for waiting on task completion
 def handler(event):
     """Retrieve and save WAS Findings from the DMZ."""
     try:
-        print('in handler')
         main()
         return {
             "statusCode": 200,
@@ -35,13 +33,9 @@ def handler(event):
 
 def main():
     """Fetch and save DMZ WAS Findings."""
-    print('in main')
     try:
         all_orgs = Organization.objects.all()
-        print(all_orgs)
-    
         # all_orgs = Organization.objects.filter(acronym__in=['USAGM', 'DHS'])
-
 
         # Step 1: Get the current date and time in UTC
         current_time = datetime.datetime.now(datetime.timezone.utc)
@@ -49,7 +43,7 @@ def main():
         days_ago = current_time - datetime.timedelta(days=15)
         # Step 3: Convert to an ISO 8601 string with timezone (e.g., UTC)
         since_timestamp_str = days_ago.date().isoformat()
-        
+
         for org in all_orgs:
             print(
                 "Processing organization: {acronym}, {name}".format(
@@ -66,7 +60,6 @@ def main():
                 data = fetch_dmz_was_findings_task(
                     org.acronym, page, per_page, since_timestamp_str
                 )
-                print(data)
                 if not data or data.get("status") != "Processing":
                     print(
                         "Failed to start Was Finding sync task for org: {acronym}, {name}".format(
@@ -88,22 +81,17 @@ def main():
                     continue
 
                 response = fetch_dmz_was_finding_data(data.get("task_id", None))
-                print(response)
                 while response and response.get("status") == "Pending":
                     time.sleep(1)
                     response = fetch_dmz_was_finding_data(data.get("task_id", None))
 
                 if response and response.get("status") == "Completed":
-                    was_finding_array = (
-                        response.get("result", {}).get("data", [])
-                    )
+                    was_finding_array = response.get("result", {}).get("data", [])
                     total_pages = response.get("result", {}).get("total_pages", 1)
                     current_page = response.get("result", {}).get("current_page", 1)
                     print("findings")
                     print(was_finding_array)
-                    save_findings_to_db(
-                        was_finding_array, org
-                    )
+                    save_findings_to_db(was_finding_array, org)
 
                     if current_page >= total_pages:
                         done = True
@@ -115,7 +103,8 @@ def main():
                         )
                     )
     except Exception as e:
-        print('failed to in main: {error}'.format(error=e))
+        print("failed to in main: {error}".format(error=e))
+
 
 def fetch_dmz_was_findings_task(org_acronym, page, per_page, since_timestamp):
     """Fetch Was Finding task id."""
@@ -170,6 +159,7 @@ def fetch_dmz_was_finding_data(task_id):
         print("Error fetching DMZ Was Finding data: {error}".format(error=e))
         return None
 
+
 def convert_timestamp_to_date(timestamp: str) -> str:
     """Convert an ISO 8601 timestamp to a date string in YYYY-MM-DD format."""
     if timestamp:
@@ -179,45 +169,53 @@ def convert_timestamp_to_date(timestamp: str) -> str:
         formatted_date = None
     return formatted_date
 
+
 def save_findings_to_db(was_finding_array, org):
     """Save WAS finding data to the mini datalake using Django ORM."""
     if was_finding_array:
         for asset in was_finding_array:
             try:
-                
                 (
                     was_finding_object,
                     created,
                 ) = WasFindings.objects.update_or_create(
-                    finding_uid= asset.get('finding_uid'),
+                    finding_uid=asset.get("finding_uid"),
                     defaults={
-                        'finding_type': asset.get('finding_type'),
-                        'webapp_id': asset.get('webapp_id'),
-                        'webapp_url': asset.get('webapp_url'),
-                        'webapp_name': asset.get('webapp_name'),
-                        'was_org_id': asset.get('was_org_id'),
-                        'name': asset.get('name'),
-                        'owasp_category': asset.get('owasp_category'),
-                        'severity': asset.get('severity'),
-                        'times_detected': asset.get('times_detected'),
-                        'cvss_v3_attack_vector': asset.get('cvss_v3_attack_vector'),
-                        'base_score': asset.get('base_score'),
-                        'temporal_score': asset.get('temporal_score'),
-                        'fstatus': asset.get('fstatus'),
-                        'last_detected': convert_timestamp_to_date(asset.get('last_detected')),
-                        'first_detected': convert_timestamp_to_date(asset.get('first_detected')),
-                        'potential': asset.get('potential'),
-                        'cwe_list': asset.get('cwe_list'),
-                        'wasc_list': asset.get('wasc_list'),
-                        'last_tested': convert_timestamp_to_date(asset.get('last_tested')),
-                        'fixed_date': convert_timestamp_to_date(asset.get('fixed_date')),
-                        'is_ignored': asset.get('is_ignored'),
-                        'is_remediated': asset.get('is_remediated'),
-                        'url': asset.get('url'),
-                        'qid': asset.get('qid'),
-                        'response': asset.get('response')
+                        "finding_type": asset.get("finding_type"),
+                        "webapp_id": asset.get("webapp_id"),
+                        "webapp_url": asset.get("webapp_url"),
+                        "webapp_name": asset.get("webapp_name"),
+                        "was_org_id": asset.get("was_org_id"),
+                        "name": asset.get("name"),
+                        "owasp_category": asset.get("owasp_category"),
+                        "severity": asset.get("severity"),
+                        "times_detected": asset.get("times_detected"),
+                        "cvss_v3_attack_vector": asset.get("cvss_v3_attack_vector"),
+                        "base_score": asset.get("base_score"),
+                        "temporal_score": asset.get("temporal_score"),
+                        "fstatus": asset.get("fstatus"),
+                        "last_detected": convert_timestamp_to_date(
+                            asset.get("last_detected")
+                        ),
+                        "first_detected": convert_timestamp_to_date(
+                            asset.get("first_detected")
+                        ),
+                        "potential": asset.get("potential"),
+                        "cwe_list": asset.get("cwe_list"),
+                        "wasc_list": asset.get("wasc_list"),
+                        "last_tested": convert_timestamp_to_date(
+                            asset.get("last_tested")
+                        ),
+                        "fixed_date": convert_timestamp_to_date(
+                            asset.get("fixed_date")
+                        ),
+                        "is_ignored": asset.get("is_ignored"),
+                        "is_remediated": asset.get("is_remediated"),
+                        "url": asset.get("url"),
+                        "qid": asset.get("qid"),
+                        "response": asset.get("response"),
                     },
                 )
-                
+
             except Exception as e:
                 print("Error saving Was Finding: {error}".format(error=e))
